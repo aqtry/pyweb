425f47378fa50646d1496fd2a16225617a638b4b
web01 <3489343349@qq.com>
2019-05-31 14:34:13 +0800

    添加login功能

--- view.py
+++ view.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import redicret
 
 def index(request):
     return HttpResonse('index')
+
+def login(request):
+    return redcret('\login')
